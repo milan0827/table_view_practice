219eee6a54fff647515d98d454777d984239d22b
milan0827 <milanrokaya2014@gmail.com>
2023-12-08 12:40:18 +0545

    Code refactored

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,56 +1,13 @@
+import { tableData } from "../../data";
 import TableRow from "./TableRow";
-
-const tableData = [
-  {
-    invoice: "#12345",
-    invoiceDate: "2021-12-12",
-    dueDate: "2021-12-24",
-    status: "Unpaid",
-    amount: "1234",
-  },
-  {
-    invoice: "#12346",
-    invoiceDate: "2022-04-01",
-    dueDate: "2022-05-24",
-    status: "Paid",
-    amount: "345",
-  },
-  {
-    invoice: "#12347",
-    invoiceDate: "2023-11-12",
-    dueDate: "2023-12-24",
-    status: "Pending",
-    amount: "785",
-  },
-  {
-    invoice: "#123458",
-    invoiceDate: "2023-12-01",
-    dueDate: "2023-12-24",
-    status: "Unpaid",
-    amount: "567",
-  },
-  {
-    invoice: "#123459",
-    invoiceDate: "2023-12-01",
-    dueDate: "2023-12-24",
-    status: "Paid",
-    amount: "221",
-  },
-  {
-    invoice: "#123450",
-    invoiceDate: "2023-12-01",
-    dueDate: "2023-12-24",
-    status: "Pending",
-    amount: "125",
-  },
-];
+// import { tableData } from "../../data";
 
 function Table({ onOpenModal }) {
   return (
     <table className="w-full border-separate border-spacing-4 border border-stone-300 text-center">
       <thead>
-        <tr className="">
-          <th className="">
+        <tr>
+          <th>
             <input type="checkbox" id="invoiceTitle" />
             <label htmlFor="invoiceTitle" className="ml-2">
               Invoice
