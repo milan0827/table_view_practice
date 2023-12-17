38ae65787c020291d3198b605ed6942e91e6c163
milan0827 <milanrokaya2014@gmail.com>
2023-12-17 20:09:34 +0545

    chore: Code refactored

--- src/components/TableHeader.jsx
+++ src/components/TableHeader.jsx
@@ -1,10 +1,11 @@
 import { GiHamburgerMenu } from "react-icons/gi";
+import LinkButton from "./Button/LinkButton";
 
-function Header() {
+function TableHeader() {
   const Mapper = ({ data }) => {
     return data.map((key) => (
       <span key={key}>
-        <GiHamburgerMenu className="text-xl" />
+        <GiHamburgerMenu className="mt-4 text-xl" />
       </span>
     ));
   };
@@ -18,10 +19,13 @@ function Header() {
         </label>
       </div>
       <div className="flex items-center justify-between gap-2">
+        <LinkButton type="submit" to="form">
+          + Add new
+        </LinkButton>
         <Mapper data={[1, 2, 3]} />
       </div>
     </header>
   );
 }
 
-export default Header;
+export default TableHeader;
