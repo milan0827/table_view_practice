ebd395c0644086c4ea97be150cbdb314a08a861a
milan0827 <milanrokaya2014@gmail.com>
2023-12-17 20:12:06 +0545

    chore: Code refactored

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,31 @@
 import { useState } from "react";
-import Modal from "../components/Modal";
-import Header from "../components/TableHeader";
 import Table from "../components/Table/Table";
 import AppHeader from "../components/AppHeader";
+import TableHeader from "../components/TableHeader";
 
 function Home() {
-  const [showModal, setShowModal] = useState(false);
+  const [datas, setDatas] = useState(
+    JSON.parse(localStorage.getItem("DATA")) || [],
+  );
 
-  function onOpenModal() {
-    setShowModal((open) => !open);
+  function handleDelete(id) {
+    const updatedData = datas.filter((data) => data.invoice !== id);
+    localStorage.setItem("DATA", JSON.stringify(updatedData));
+    setDatas(updatedData);
   }
 
   return (
-    <div className="shadow-black-500 relative flex h-screen w-full flex-col items-center justify-start bg-gray-100 p-20 shadow-2xl">
+    <div className="shadow-black-500 relative flex w-full flex-col items-center justify-start bg-gray-100 p-20 ">
       <AppHeader type="noError">Invoice Details</AppHeader>
-      <Header />
-      <Table onOpenModal={() => setShowModal(true)} />
-      {showModal && <Modal onOpenModal={onOpenModal} />}
+
+      <TableHeader />
+      {datas.length === 0 ? (
+        <p className="text-3xl font-semibold text-red-400">
+          No data to view! Please add data
+        </p>
+      ) : (
+        <Table datas={datas} handleDelete={handleDelete} />
+      )}
     </div>
   );
 }
