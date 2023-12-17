d290bd8db2c084088b3dccf1a5a2cd64f5f602ce
milan0827 <milanrokaya2014@gmail.com>
2023-12-17 20:07:03 +0545

    fix: code refactored

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,11 @@
 import { Button } from "./Button/Button";
 
-function Modal({ onOpenModal }) {
+function Modal({ data, handleDelete, handleOpenModal, setShowModal }) {
   return (
-    <>
+    <div key={data.invoice}>
       <div className="absolute right-[35%] top-[25%] z-20 flex flex-col rounded-md bg-gray-200 p-[6rem]">
         <span
-          onClick={onOpenModal}
+          onClick={() => setShowModal(false)}
           role="button"
           className="absolute right-4 top-0 cursor-pointer text-4xl text-gray-500 hover:text-red-400"
         >
@@ -15,13 +15,26 @@ function Modal({ onOpenModal }) {
           Are you Sure you want to delete ?
         </p>
         <div className="mt-8 flex items-center justify-between">
-          <Button type="yes">Yes</Button>
-          <Button type="no">No</Button>
+          <Button
+            type="confirm"
+            onClick={() => {
+              handleDelete(data.invoice);
+              handleOpenModal(false);
+            }}
+          >
+            Yes
+          </Button>
+          <Button type="delete" onClick={() => setShowModal(false)}>
+            No
+          </Button>
         </div>
       </div>
 
-      <span onClick={onOpenModal} className="overlay absolute inset-0 "></span>
-    </>
+      <span
+        onClick={() => setShowModal(false)}
+        className="overlay absolute inset-0 "
+      ></span>
+    </div>
   );
 }
 
