76248215aaa8901cbdaf2f0cf96147413e531bc8
milan0827 <milanrokaya2014@gmail.com>
2023-12-17 20:02:12 +0545

    fix: styles added to edit button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,15 @@
-export function Button({ children, type, onOpenModal }) {
+export function Button({ children, type, onClick }) {
   const base = "rounded-md px-4 py-1 font-semibold text-white ";
 
   const styles = {
+    edit: base + "bg-amber-300 ",
     delete: base + "bg-red-400",
-    yes: base + "bg-amber-300",
-    no: base + "bg-red-400",
+    confirm: base + "bg-amber-300",
     submit: base + "bg-blue-400 w-full py-2 mt-4",
   };
 
   return (
-    <button className={styles[type]} onClick={onOpenModal}>
+    <button className={styles[type]} onClick={onClick}>
       {children}
     </button>
   );
