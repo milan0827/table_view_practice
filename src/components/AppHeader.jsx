673b868b17c3a86836027ba469974ddc2ebb2b2b
milan0827 <milanrokaya2014@gmail.com>
2023-12-17 20:01:10 +0545

    fix: title passed as props not as children

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,13 +1,12 @@
-function AppHeader({ children, type }) {
-  const base =
-    "mb-4 text-5xl font-semibold uppercase text-stone-800 text-center mt-4 ";
+function AppHeader({ type, title }) {
+  const base = "text-5xl font-semibold uppercase text-stone-800 text-center ";
 
   const styles = {
-    error: base + "text-red-500",
+    error: base + " text-red-500",
     noError: base + "text-stone-800",
   };
 
-  return <h1 className={styles[type]}>{children}</h1>;
+  return <h1 className={styles[type]}>{title}</h1>;
 }
 
 export default AppHeader;
