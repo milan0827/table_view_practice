8726e93d275e15faf6261df488e3eb7c7e134f81
milan0827 <milanrokaya2014@gmail.com>
2023-12-18 10:39:06 +0545

    fix: Removed edit property from style as it same as the confirm property

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,6 @@ export function Button({ children, type, onClick }) {
   const base = "rounded-md px-4 py-1 font-semibold text-white ";
 
   const styles = {
-    edit: base + "bg-amber-300 ",
     delete: base + "bg-red-400",
     confirm: base + "bg-amber-300",
     submit: base + "bg-blue-400 w-full py-2 mt-4",
