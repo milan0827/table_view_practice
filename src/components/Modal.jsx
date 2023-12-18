6619a01357aa30ca20d99f4553f399fa558520b9
milan0827 <milanrokaya2014@gmail.com>
2023-12-18 11:12:09 +0545

    fix: Code fixed

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,39 +2,48 @@ import { Button } from "./Button/Button";
 
 function Modal({ data, handleDelete, handleOpenModal, setShowModal }) {
   return (
-    <div key={data.invoice}>
-      <div className="absolute right-[35%] top-[25%] z-20 flex flex-col rounded-md bg-gray-200 p-[6rem]">
-        <span
-          onClick={() => setShowModal(false)}
-          role="button"
-          className="absolute right-4 top-0 cursor-pointer text-4xl text-gray-500 hover:text-red-400"
-        >
-          &times;
-        </span>
-        <p className="text-lg text-gray-600">
-          Are you Sure you want to delete ?
-        </p>
-        <div className="mt-8 flex items-center justify-between">
-          <Button
-            type="confirm"
-            onClick={() => {
-              handleDelete(data.invoice);
-              handleOpenModal(false);
-            }}
+    <>
+      <div
+        key={data.invoice}
+        className="absolute right-[40%] top-1/2 z-20 flex flex-col rounded-md bg-gray-200 "
+      >
+        <div className="flex flex-col px-4">
+          <span
+            onClick={() => setShowModal(false)}
+            role="button"
+            className="cursor-pointer self-end text-4xl text-gray-500 hover:text-red-400"
           >
-            Yes
-          </Button>
-          <Button type="delete" onClick={() => setShowModal(false)}>
-            No
-          </Button>
+            &times;
+          </span>
+
+          <div className="mt-8 flex flex-col items-center gap-10 px-10 pb-10 pt-5">
+            <p className="text-lg text-gray-600">
+              Are you Sure you want to delete ?
+            </p>
+
+            <div className="flex gap-20">
+              <Button
+                type="confirm"
+                onClick={() => {
+                  handleDelete(data.invoice);
+                  handleOpenModal(false);
+                }}
+              >
+                Yes
+              </Button>
+              <Button type="delete" onClick={() => setShowModal(false)}>
+                No
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
 
       <span
         onClick={() => setShowModal(false)}
-        className="overlay absolute inset-0 "
+        className="overlay absolute inset-0 h-screen"
       ></span>
-    </div>
+    </>
   );
 }
 
