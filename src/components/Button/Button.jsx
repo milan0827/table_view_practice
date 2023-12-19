5259339165143671af2c955e01b3c1bc5a628a41
milan0827 <milanrokaya2014@gmail.com>
2023-12-19 10:24:55 +0545

    feat: disabled feature added when submitting form

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,4 @@
-export function Button({ children, type, onClick }) {
+export function Button({ children, type, onClick, disabled, className }) {
   const base = "rounded-md px-4 py-1 font-semibold text-white ";
 
   const styles = {
@@ -6,6 +6,17 @@ export function Button({ children, type, onClick }) {
     confirm: base + "bg-amber-300",
     submit: base + "bg-blue-400 w-full py-2 mt-4",
   };
+
+  if (disabled)
+    return (
+      <button
+        className={`${styles[type]} ${className}`}
+        onClick={onClick}
+        disabled={disabled}
+      >
+        {children}
+      </button>
+    );
 
   return (
     <button className={styles[type]} onClick={onClick}>
