75d6e84620c9576d74352b984014f3bc844e2b05
milan0827 <milanrokaya2014@gmail.com>
2023-12-21 14:53:19 +0545

    refactor: Context value passed

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import InvoiceProvider from "./contexts/invoiceContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <InvoiceProvider>
+      <App />
+    </InvoiceProvider>
   </React.StrictMode>,
 );
