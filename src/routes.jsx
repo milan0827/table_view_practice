29755d9558a0371ed05bc057002547a3015293f8
milan0827 <milanrokaya2014@gmail.com>
2023-12-21 14:58:35 +0545

    feat: Invalid route error added

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Form from "./components/Form";
+import InvalidRouteError from "./pages/InvalidRouteError";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    errorElement: <InvalidRouteError />,
   },
   {
     path: "form",
