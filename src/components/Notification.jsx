c6bb5b2b68a2d1a12cea61d4f710b382780c2d0d
milan0827 <milanrokaya2014@gmail.com>
2023-12-22 07:45:34 +0545

    fix: Removed unnecessary code

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,21 +1,6 @@
-import { useEffect } from "react";
-import { useInvoice } from "../contexts/invoiceContext";
 import { createPortal } from "react-dom";
 
 function Message({ children }) {
-  const { getData, setIsDelete } = useInvoice();
-
-  useEffect(
-    function () {
-      setTimeout(() => {
-        setIsDelete(false);
-      }, 2000);
-    },
-    [getData],
-  );
-
-  console.log(getData);
-
   return createPortal(
     <p className="absolute right-[1%] top-[5%] z-[1000] rounded-md bg-green-400/80 px-20 py-4 text-lg text-stone-100 shadow-lg shadow-green-500/60 transition-colors duration-300">
       {children}
